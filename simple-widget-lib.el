80bd35d38c8c4a3cb9817aff3d30277bc13476a8
Jacob Salzberg <jssalzbe@ncsu.edu>
2019-01-07 11:40:34 -0500

    Start simple widget lib

---
+++ simple-widget-lib.el
@@ -0,0 +1,30 @@
+;;; simple-widget-lib.el --- A simple widget library for creating interactive documents
+
+;; Copyright © 2019 Jacob Salzberg
+
+;; Author: Jacob Salzberg <jssalzbe@ncsu.edu>
+;; URL: https://github.com/jsalzbergedu/simple-widget-lib-emacs
+;; Version: 0.1.0
+;; Keywords: widget library simple interactive
+
+;; This file is not a part of GNU Emacs
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+
+;;; simple-widget-lib.el ends here
